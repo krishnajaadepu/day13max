f5b059222641b6d555d8a75d6e04236fed99c8a8
krishnajaadepu <113959348+krishnajaadepu@users.noreply.github.com>
2022-12-21 11:08:28 +0530

    Update Generics.java

--- com/bridgelabz/Generics.java
+++ com/bridgelabz/Generics.java
@@ -3,34 +3,41 @@ package com.bridgelabz;
 public class Generics <T extends Comparable>{
 
 	 // type parameter extend comparable class bcz there is no compareTo method in type parameter T
-    T[] array;
 
-Generics(T[] arr1) {   // parameterized constructor is used to initialize instance variable of objects
-    this.array = arr1;
-}
+    T variable1;
+    T variable2;
+    T variable3;
 
-void maximumOfVariables(){
-    T largest = this.array[0];
-  for (int i=0; i<this.array.length; i++){
-      if(this.array[i].compareTo(largest) > 0){
-          largest = this.array[i];
-          this.array[i] = largest;
-      }
-  }
-  System.out.println(" Largest element is " + largest);
-}
+    Generics(T var1, T var2, T var3){   // parameterized constructor is used to initialize instance variable of objects
+        this.variable1 = var1;
+        this.variable2 = var2;
+        this.variable3 = var3;
+    }
 
-public static void main(String[] args) {
-    Integer[] arr1 = {12,4,7,10,6};
-    Generics <Integer> obj1 = new Generics(arr1);
-    obj1.maximumOfVariables();
+    void maximumOfVariables(){
+        if(this.variable1.compareTo(this.variable2) > 0){
+            if(this.variable1.compareTo(this.variable3) > 0){
+                System.out.println( this.variable1 + " is Maximum ");
+            }else {
+                System.out.println( this.variable3 + " is greater ");
+            }
+        }else{
+            if(this.variable2.compareTo(this.variable3) > 0){
+                System.out.println( this.variable2 + " is maximum ");
+            }else{
+                System.out.println( this.variable3 + " is maximum ");
+            }
+        }
+    }
 
-    String[] arr2 = {"kavya", "jaya", "ganesh"};
-    Generics <String> obj2 = new Generics(arr2);
-    obj2.maximumOfVariables();
+    public static void main(String[] args) {
+       Generics <Integer> obj1 = new Generics(4,5,66);
+       obj1.maximumOfVariables();
 
-    Float[] arr3 = {2.1f, 5.4f, 6.0f};
-    Generics <Float> obj3 = new Generics(arr3);
-    obj3.maximumOfVariables();
-}
+       Generics <String> obj2 = new Generics("mango", "apple", "pineapple");
+       obj2.maximumOfVariables();
+
+       Generics <Float> obj3 = new Generics(1.1f,2.3f,33f);
+       obj3.maximumOfVariables();
+    }
 }
